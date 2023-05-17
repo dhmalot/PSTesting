0fb560c8c99704bec8b750f3004e3b0a78f401a4
Katamreddy <Vamshidhar.Katamreddy@sony.com>
2023-05-17 21:22:43 +0530

    report added

--- Demo.py
+++ Demo.py
@@ -6,21 +6,17 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path= r"C:\Users\7000033861\Downloads\chromedriver_win32\chromedriver.exe")
 
-driver.get("https://www.playstation.com/en-in/")
+driver.get("https://www.airindia.com/?utm_source=google&utm_medium=cpc&utm_campaign=brand-exact&gad=1&gclid=EAIaIQobChMI7ceVsIj8_gIVW5hmAh0C8QO7EAAYASAAEgJU1vD_BwE")
 driver.maximize_window()
 driver.delete_all_cookies()
-driver.find_element(By.ID, "menu-button-primary--msg-games").click()
+driver.find_element(By.XPATH, "//img[@title = 'Air India Logo']").click()
 time.sleep(10)
-landingpage_title = driver.title
-print(landingpage_title)
+#landingpage_title = driver.title
+#print(landingpage_title)
 # elements = driver.find_elements(By.XPATH,"//div[@class='shared-nav__secondary-parent shared-nav__secondary-parent--msg_games']//section[@aria-label='Dropdown Menu']/div[@class='shared-nav__secondary-item']")
 # games_category_elements = []
 # for ele in elements:
 #     text = ele.text
 #     games_category_elements.append(text)
 # print(games_category_elements)
-driver.find_element(By.XPATH, "//a[@id = 'link-secondary--msg-ps5']//span[text() = 'PS5']").click()
-time.sleep(10)
-PS5_title = driver.title
-print(PS5_title)
-time.sleep(10)
+#
